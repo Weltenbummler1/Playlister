d21b01dafdd0a98df0fe13b389546d65a4137810
Dirk Moreno <dirk@shiprocker.com>
2022-05-01 21:55:19 +0200

    Users can see all playlists2

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask,  render_template
+from flask_pymongo import PyMongo
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+app.config["MONGO_URI"] = "mongodb+srv://dubliner73:test123@cluster0.klq8d.mongodb.net/Idealista?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+playlists = list(mongo.db.scrapy_items.find({}).sort("rent_range"))
+
+
 
 #@app.route('/')
 #def index():
@@ -8,10 +16,10 @@ app = Flask(__name__)
 #    return render_template('home.html', msg='Flask is Cool!!')
 
 # OUR MOCK ARRAY OF PROJECTS
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
+#playlists = [
+#    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+#    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+#]
 
 @app.route('/')
 def playlists_index():
